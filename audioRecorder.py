ad2042947b4e0a05615f62359d6eb8d25f901c69
Martin Šponiar <sponiarmartin2000@gmail.com>
2025-07-07 17:28:00 +0200

    Updated Audio Recorder

--- audioRecorder.py
+++ audioRecorder.py
@@ -4,16 +4,52 @@ import numpy as np
 import scipy as sc
 from scipy.io.wavfile import write
 
-# Sampling frequency
-freq = 44100
-# Recording duration
-duration = 5
-# Channel
-channels = 2
+class AudioRecorder:
+    def __init__(self):
+        # Sampling frequency
+        self.freq = 44100
+        # Recording duration
+        self.duration = 5
+        # Channel
+        self.channels = 2
+        self.isRecording = False
+        self.recording = None
+        self.stopped = False
 
-print("Recording...")
-recording = sd.rec(int(duration * freq),samplerate=freq,channels=channels)
-sd.wait()
-print("Recording finished")
-write("recording1.wav",freq,recording)
-#w.write("recording2.wav", recording, freq,sampwidth=2  )
+    def start(self, duration):
+        if self.isRecording:
+            print("Recording already started.")
+            return False
+
+        print("Recording...")
+        self.isRecording = True
+        try:
+            self.recording = sd.rec(int(duration * self.freq),samplerate=self.freq,channels=self.channels)
+            sd.wait()
+            if not self.stopped:
+                return True, "Recording finished."
+            else:
+                return True, "Recording stopped."
+        except Exception as e:
+            self.isRecording = False
+            return False, f"Failed to start recording: {e}"
+        finally:
+            self.isRecording = False
+
+    def stop(self):
+        self.stopped = True
+        self.isRecording = False
+        sd.stop()
+
+    def save(self, filename):
+        if self.recording:
+            return False, "Nothing to save"
+        if filename.endswith(".wav"):
+            filename += ".wav"
+
+        try:
+            write(filename,self.freq,self.recording)
+            #w.write(filename, recording, freq, sampwidth=2)
+            return True, "Saved recording"
+        except Exception as e:
+            return False, f"Failed to save recording: {e}"
